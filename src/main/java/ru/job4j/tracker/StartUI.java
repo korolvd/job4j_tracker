92960e65806aeec9504321f8db15189c21c9aaaf
korolvd <ffirin@mail.ru>
2021-01-11 01:35:13 +0500

    9. toString [#423087]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,10 +3,8 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        item.setName("Tracker - хранилище");
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        Item exist = tracker.findById(1);
-        System.out.println(exist.getId() + ". " + exist.getName());
+        item.setName("9. toString");
+        item.setId(423087);
+        System.out.println(item.toString());
     }
 }
