f405bc64c96e7b2b6a330559a362b0bc54b58330
korolvd <ffirin@mail.ru>
2021-04-19 21:15:05 +0500

    2.2. Лямбда блок [#422812]

---
+++ src/main/java/ru/job4j/lamda/LambdaUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.lamda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right);
+            return left.compareTo(right);
+        };
+    }
+}
