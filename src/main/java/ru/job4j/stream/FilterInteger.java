f684a143cd46660a03b505b8481acf5aa015775f
korolvd <ffirin@mail.ru>
2021-04-20 20:50:50 +0500

    0. Stream API [#422819]

--- src/main/java/ru/job4j/stream/FilterInteger.java
+++ src/main/java/ru/job4j/stream/FilterInteger.java
@@ -9,7 +9,7 @@ public class FilterInteger {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10, -23, 132, -45, 2324));
         List<Integer> filterList = list.stream().filter(
-                integer -> integer >= 0
+                integer -> integer > 0
         ).collect(Collectors.toList());
         System.out.println(filterList);
     }
