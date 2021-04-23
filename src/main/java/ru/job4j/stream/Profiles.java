df76765e37c178e0845740d102d3ac3e301cdeeb
korolvd <ffirin@mail.ru>
2021-04-23 10:55:03 +0500

    3. Уникальность элементов и сортировка. [#422822]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,15 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        List<Address> addresses = profiles.stream()
+                .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
+        Comparator<Address> comparator = Comparator.comparing(Address::getCity);
+        return addresses.stream().sorted(comparator).distinct().collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class ProfilesTest {
 
     @Test
-    public void collect() {
+    public void whenCollect() {
         List<Profile> profiles = List.of(
                 new Profile(new Address("Moscow", "Lenin street", 15, 8)),
                 new Profile(new Address("Voronej", "Moscow street", 105, 18)),
@@ -21,10 +21,32 @@ public class ProfilesTest {
         Profiles pf = new Profiles();
         List<Address> rsl = pf.collect(profiles);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("Moscow", "Lenin street", 15, 8));
-        expected.add(new Address("Voronej", "Moscow street", 105, 18));
-        expected.add(new Address("Piter", "K. Marks street", 3, 56));
         expected.add(new Address("Ijevsk", "Green street", 232, 72));
+        expected.add(new Address("Moscow", "Lenin street", 15, 8));
+        expected.add(new Address("Piter", "K. Marks street", 3, 56));
+        expected.add(new Address("Voronej", "Moscow street", 105, 18));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectWithoutDuplicates() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Lenin street", 15, 8)),
+                new Profile(new Address("Voronej", "Moscow street", 105, 18)),
+                new Profile(new Address("Moscow", "Lenin street", 15, 8)),
+                new Profile(new Address("Voronej", "Moscow street", 105, 18)),
+                new Profile(new Address("Piter", "K. Marks street", 3, 56)),
+                new Profile(new Address("Ijevsk", "Green street", 232, 72)),
+                new Profile(new Address("Piter", "K. Marks street", 3, 56)),
+                new Profile(new Address("Ijevsk", "Green street", 232, 72))
+        );
+        Profiles pf = new Profiles();
+        List<Address> rsl = pf.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Ijevsk", "Green street", 232, 72));
+        expected.add(new Address("Moscow", "Lenin street", 15, 8));
+        expected.add(new Address("Piter", "K. Marks street", 3, 56));
+        expected.add(new Address("Voronej", "Moscow street", 105, 18));
         assertThat(rsl, is(expected));
     }
 }
