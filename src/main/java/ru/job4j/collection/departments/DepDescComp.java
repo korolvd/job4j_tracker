a9a8daa734302eaafa720aed0271ac35e6adc9c3
korolvd <ffirin@mail.ru>
2021-04-24 17:13:24 +0500

    2. Отсортировать департаменты [#422977]

---
+++ src/main/java/ru/job4j/collection/departments/DepDescComp.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection.departments;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.split("/")[0].compareTo(o1.split("/")[0]);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.collection.departments;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DepartmentsTest {
+    @Test
+    public void whenMissed() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAscSorted() {
+        List<String> input = Arrays.asList("k2",
+                "k1/sk1",
+                "k2/sk1",
+                "k1"
+        );
+        List<String> expect = Arrays.asList("k1",
+                "k1/sk1",
+                "k2",
+                "k2/sk1"
+        );
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenDescSorted() {
+        List<String> input = Arrays.asList("k1",
+                "k1/sk2",
+                "k1/sk1",
+                "k2/sk2",
+                "k2/sk1",
+                "k2"
+        );
+        List<String> expect = Arrays.asList("k2",
+                "k2/sk1",
+                "k2/sk2",
+                "k1",
+                "k1/sk1",
+                "k1/sk2"
+        );
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
+    }
+}
